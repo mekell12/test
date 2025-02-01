aa15c12078f514b298e68084457654f695713231
mekell12 <seeders_rats_1w@icloud.com>
2025-02-01 12:58:56 -0800

    Create Main.java as a test file

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Just another test file, named Main to meet Java requirements
+public class Main {
+  String helloWorld = "Hello world";
+  public static void main(String[] args) {
+    System.out.println(helloWorld);
+  }
+}
